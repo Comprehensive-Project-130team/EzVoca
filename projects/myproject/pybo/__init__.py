8f3fd807e056e31840b08e890fe1f145da18f94f
kjs2016 <38197122+kjs2016@users.noreply.github.com>
2021-05-05 01:40:15 +0900

    Update __init__.py

--- projects/myproject/pybo/__init__.py
+++ projects/myproject/pybo/__init__.py
@@ -27,5 +27,8 @@ def create_app():
     app.register_blueprint(auth_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
-
+    
+    from .filter import format_datetime
+    app.jinja_env.filters['datetime'] = format_datetime
+    
     return app
