33a08ba961312899c91d8a6dc0ee6aafd9af2128
unknown <egpg0926@gmail.com>
2021-05-18 01:45:23 +0900

    voca_create, word, word_test html 추가, 그 외 html, css 수정

--- projects/myproject/pybo/filter.py
+++
@@ -1,5 +0,0 @@
-import locale
-locale.setlocale(locale.LC_ALL, '')
-
-def format_datetime(value, fmt='%Y년 %m월 %d일 %H:%M'):
-    return value.strftime(fmt)
--- projects/myproject/pybo/static/css/dashboard.js
+++
@@ -1,8 +0,0 @@
- /* globals feather:false */
-//사이드바 아이콘 js
-(function () {
-  'use strict'
-
-  feather.replace()
-
-})()
